fe69541c3ad0eefdf91bf3a34680456778fff428
Benny <bennykurian02@gmail.com>
2022-06-10 00:36:58 +0200

    Functional implementation TDD

--- ScoreCardApp/src/main/java/ScoreCardApp.java
+++ ScoreCardApp/src/main/java/ScoreCardApp.java
@@ -1,13 +1,12 @@
 import Model.Match;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ScoreCardApp {
-    private final Map<Integer, Match> matchMap = new HashMap<>();
+    private final Map<Integer, Match> matchMap = new LinkedHashMap<>();
 
     //Method to create and start match
-//Create Entry in to the map when match start
     public void createMatch(int matchID, String homeTeam, String opponents) {
         //Add match to the map with score as 0 0
         matchMap.put(matchID, new Match(homeTeam, opponents, 0, 0));
@@ -20,15 +19,26 @@ public class ScoreCardApp {
 
     //Method to finish match , remove match from the map
     public void finishMatch(int matchId) {
-        if (matchMap.containsKey(matchId)){
+        //check if match exist in the system if so remove
+        if (matchMap.containsKey(matchId)) {
             matchMap.remove(matchId);
         }
     }
-//updating match score based on the input
+
+    //updating match score based on the input
     public void updateScoreCard(int matchId, int homeScore, int opponentScore) {
+        //check if match exist then update with latest score
         if (matchMap.containsKey(matchId)) {
             matchMap.get(matchId).setHomeScore(homeScore);
             matchMap.get(matchId).setOpponentScore(opponentScore);
         }
+    }
+
+    public List<Match> getMatchOverview() {
+        //Sorting map based on the score total then reversing it to get based on decending total and input order last to first
+        List<Match> matchList = matchMap.values().stream().sorted(Comparator.comparingInt(match -> (match.getHomeScore() + match.getOpponentScore())))
+                .collect(Collectors.toList());
+        Collections.reverse(matchList);
+        return matchList;
     }
 }
--- ScoreCardApp/src/test/java/ScoreCardAppTest.java
+++ ScoreCardApp/src/test/java/ScoreCardAppTest.java
@@ -1,4 +1,8 @@
+import Model.Match;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScoreCardAppTest {
@@ -29,8 +33,33 @@ public class ScoreCardAppTest {
         assertEquals(2,scoreCardApp.getMatchDetails(1).getHomeScore());
 
     }
+    @Test
+    public void getMatchOverviewTest(){
+        ScoreCardApp scoreCardApp=new ScoreCardApp();
+        createMultipleMatch(scoreCardApp);
+        scoreCardApp.updateScoreCard(1,0,5);
+        scoreCardApp.updateScoreCard(3,10,2);
+        scoreCardApp.updateScoreCard(7,2,2);
+        scoreCardApp.updateScoreCard(4,6,6);
+        scoreCardApp.updateScoreCard(2,3,1);
+        List<Match> matchList=scoreCardApp.getMatchOverview();
+        assertEquals("Uruguay",matchList.get(0).getHome());
+        assertEquals("Italy",matchList.get(0).getOpponent());
+        assertEquals("Spain",matchList.get(1).getHome());
+        assertEquals("Brazil",matchList.get(1).getOpponent());
+        assertEquals("Germany",matchList.get(4).getHome());
+        assertEquals("France",matchList.get(4).getOpponent());
+    }
 
     private void createMatch(ScoreCardApp scoreCardApp) {
         scoreCardApp.createMatch(1,"Mexico","Canada");
+    }
+    private void createMultipleMatch(ScoreCardApp scoreCardApp)
+    {
+        scoreCardApp.createMatch(1,"Mexico","Canada");
+        scoreCardApp.createMatch(3,"Spain","Brazil");
+        scoreCardApp.createMatch(7,"Germany","France");
+        scoreCardApp.createMatch(4,"Uruguay","Italy");
+        scoreCardApp.createMatch(2,"Argentina","Australia");
     }
 }
